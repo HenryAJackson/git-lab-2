bb2ab73b5d73d274d4885c6698c904f45b336f2e
HenryAJackson <hj408322@ohio.edu>
2023-09-05 17:27:09 -0400

    adding program to git lab 2

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Henry Jackson
+ *        Date: 9/5/2023
+ * Description: code copied from cs2400 lab 2 in order to practice git.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
